a6c9bf90d7c5038a8800b459f6b660dad671b14a
winnerezy <eragbonikpeya@gmail.com>
2025-01-25 22:23:36 +0100

    potential fix

--- src/main/java/com/winnerezy/Veo/services/AuthenticationService.java
+++ src/main/java/com/winnerezy/Veo/services/AuthenticationService.java
@@ -3,8 +3,6 @@ package com.winnerezy.Veo.services;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,15 +14,16 @@ import com.winnerezy.Veo.dto.RegisterDTO;
 import com.winnerezy.Veo.models.User;
 import com.winnerezy.Veo.repositories.UserRepository;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Service
 public class AuthenticationService {
 
     private final UserRepository userRepository;
-
+    
     private final PasswordEncoder passwordEncoder;
-
+    
     private final AuthenticationManager authenticationManager;
 
     private final JwtService jwtService;
@@ -32,11 +31,12 @@ public class AuthenticationService {
     private final UserDetailsService userDetailsService;
 
     public AuthenticationService(
-            UserRepository userRepository,
-            AuthenticationManager authenticationManager,
-            PasswordEncoder passwordEncoder,
-            JwtService jwtService,
-            UserDetailsService userDetailsService) {
+        UserRepository userRepository,
+        AuthenticationManager authenticationManager,
+        PasswordEncoder passwordEncoder,
+        JwtService jwtService,
+        UserDetailsService userDetailsService
+    ) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -46,7 +46,7 @@ public class AuthenticationService {
 
     public User register(RegisterDTO registerDTO) {
 
-        if (userRepository.existsByEmail(registerDTO.getEmail())) {
+        if(userRepository.existsByEmail(registerDTO.getEmail())){
             throw new IllegalArgumentException("Email already exists");
         }
 
@@ -59,36 +59,32 @@ public class AuthenticationService {
         return userRepository.save(user);
     }
 
-    public User authenticate(LoginDTO loginDTO, HttpServletResponse response) {
+    public User authenticate(LoginDTO loginDTO, HttpServletResponse response){
 
         User user = userRepository.findByEmail(loginDTO.getEmail())
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword()));
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword()));
 
         String jwtToken = jwtService.generateToken(user);
 
-        ResponseCookie cookie = ResponseCookie.from("token", jwtToken)
-                .httpOnly(true)
-                .path("/")
-                .secure(true)
-                .sameSite("None")
-                .maxAge((int) jwtService.getExpiration())
-                .build();
+        Cookie cookie = new Cookie("token", jwtToken);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setMaxAge((int) jwtService.getExpiration());
 
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-
+        response.addCookie(cookie);
         return user;
     }
 
-    public boolean verifyToken(String token) {
+    public boolean verifyToken(String token){
         Optional<User> user = userRepository.findByEmail(jwtService.extractUsername(token));
 
-        if (user.isEmpty()) {
+        if(user.isEmpty()){
             return false;
         }
         return jwtService.isTokenValid(token, userDetailsService.loadUserByUsername(jwtService.extractUsername(token)));
     }
-
+    
 }
